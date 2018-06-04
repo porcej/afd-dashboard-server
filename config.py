d9d8a3751b4814b6a1ab56855a69a6800a5ac296
Joe Porcelli <porcej@gmail.com>
2018-06-04 00:35:16 -0400

    Cleaned up config.py a little

---
+++ config.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: ascii -*-
+
+"""
+Flask Blueprint to handle Telestaff
+
+Changelog:
+    - 2018-05-15 - Initial Commit
+
+"""
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'this is a secret key that you will never guess'
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Set this variable to "threading", "eventlet" or "gevent" to test the
+    # different async modes, or leave it set to None for the application to choose
+    # the best option based on installed packages.
+    ASYNC_MODE = None
+
+    ACTIVE_911_DEVICE_ID = ""
+
+    TS_USER = ''
+    TS_PASS = ''
+    D_USER = ''
+    D_PASS = ''
+
+    LOGGING_PATH = 'log'
