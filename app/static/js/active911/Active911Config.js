4924e26c80b04e37598b870f4d1059c13755bad4
Joe Porcelli <porcej@gmail.com>
2019-03-03 12:34:43 -0500

    Pruned Active911 Client code

--- app/static/js/active911/Active911Config.js
+++
@@ -1,8 +0,0 @@
-var Active911Config = {
-	access_uri            :  "https://access.active911.com/interface/web_api.php",
-	markerfactory_uri     :  "https://markerfactory.active911.com",
-	xmpp_server           :  "push.active911.com",
-	xmpp_domain           :  "push.active911.com",
-	xmpp_conference_name  :  "conference.push.active911.com",
-	google_maps_uri       :  "https://maps.googleapis.com/maps/api/js?client=gme-active911inc&v=3&sensor=false&channel=PRODUCTION"
-}
